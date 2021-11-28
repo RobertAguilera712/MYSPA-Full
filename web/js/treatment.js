9530370e3fba04f8b7a887f5f59fdc652a504a36
Carlos Vargas <varlopecar@gmail.com>
2021-11-27 21:55:38 -0600

    Frontend Tratamiento

---
+++ web/js/treatment.js
@@ -0,0 +1,42 @@
+function loadTreatmentsTable() {
+    loadModuleTable("treatment");
+}
+
+function searchTreatment() {
+    search("treatment");
+}
+
+function refreshTreatmentesTable() {
+    refreshTable("treatment");
+}
+
+function loadTreatmentesForm() {
+    loadModuleForm("treatment");
+}
+
+function saveTreatment(e) {
+    e.preventDefault();
+    const treatment = getTreatmentFromForm();
+    saveGet(treatment, "treatment");
+}
+
+function getTreatmentFromForm() {
+    const id = document.getElementById("txtId").value;
+    const name = document.getElementById("txtName").value;
+    const description = document.getElementById("txtDescription").value;
+    const cost = document.getElementById("txtCost").value;
+
+    const treatment = {
+        "id": id,
+        "nombre": name,
+        "descripcion": description,
+        "costo": cost,
+        "estatus": "1"
+    };
+
+    if (treatment.id.length === 0) {
+        delete treatment.id;
+    }
+
+    return treatment;
+}
